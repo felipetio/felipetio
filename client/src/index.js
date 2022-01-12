7ec2293a1915c99c29553c043ae07e2862a3ad5b
Felipe Vieira <me@felipet.io>
2022-01-12 10:57:41 -0300

    Add sentry integration (react)

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+Sentry.init({
+  dsn: "https://37eed22a13bd422eaea49d3e83ddd8c8@o1104349.ingest.sentry.io/6145862",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// Can also use with React Concurrent Mode
+// ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
 
 ReactDOM.render(
   <React.StrictMode>
